c9fe7ca843545712dc2c0337d29383e085a4c75e
Ali <zaabat97@gmail.com>
2024-07-01 09:47:34 +0200

    initial commit

---
+++ src/main/java/com/example/p3portaillocataireback/P3PortailLocataireBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.p3portaillocataireback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3PortailLocataireBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P3PortailLocataireBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/p3portaillocataireback/P3PortailLocataireBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.p3portaillocataireback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3PortailLocataireBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
