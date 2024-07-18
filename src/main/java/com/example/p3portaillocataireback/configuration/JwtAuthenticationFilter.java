4aed26461b81bc587ad80577c57c59afd855716a
Ali <zaabat97@gmail.com>
2024-07-19 00:09:52 +0200

    feat: implement an interface on JwtAuthenticationFilter on progress and add logger library done

--- src/main/java/com/example/p3portaillocataireback/configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/example/p3portaillocataireback/configuration/JwtAuthenticationFilter.java
@@ -2,11 +2,14 @@ package com.example.p3portaillocataireback.configuration;
 
 import com.example.p3portaillocataireback.exceptions.UnauthorizedRequestException;
 import com.example.p3portaillocataireback.repository.TokenRepository;
+import com.example.p3portaillocataireback.services.interfaces.JwtAuthenticationFilterInterface;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,13 +24,13 @@ import java.io.IOException;
 import java.util.List;
 
 
-
 @Component
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter implements JwtAuthenticationFilterInterface {
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
     private final TokenRepository tokenRepository;
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
     private final List<String> WHITE_LIST_URL = List.of("/api/auth/register",
             "/api/auth/login",
             "/api/rentals",
@@ -37,26 +40,30 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final AntPathMatcher pathMatcher = new AntPathMatcher();
 
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request,
-                                    @NonNull HttpServletResponse response,
-                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+    public void doFilterInternal(@NonNull HttpServletRequest request,
+                                 @NonNull HttpServletResponse response,
+                                 @NonNull FilterChain filterChain) throws ServletException, IOException {
 
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
 
+        logger.info("Processing request to URL: {}", request.getRequestURI());
+
         if (isPublicUrl(request.getRequestURI())) {
+            logger.info("Public URL accessed: {}", request.getRequestURI());
             filterChain.doFilter(request, response);
         } else {
             if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-                throw new UnauthorizedRequestException(" Authorization header is missing or invalid");
-            }
+                logger.warn("Authorization header is missing or invalid");
+                throw new UnauthorizedRequestException("Authorization header is missing or invalid");            }
             jwt = authHeader.substring(7);
 
             try {
                 userEmail = jwtService.extractUsername(jwt);
             } catch (Exception e) {
-                throw new UnauthorizedRequestException(" User not found");
+                logger.error("Invalid JWT: {}", e.getMessage());
+                throw new UnauthorizedRequestException("User not found");
             }
 
             if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
