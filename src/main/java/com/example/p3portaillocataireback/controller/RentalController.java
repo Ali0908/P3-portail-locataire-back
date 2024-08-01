b48735c653caf85ae04c1c119fbf0a676446b0b5
Ali0908 <zaabat97@gmail.com>
2024-08-01 14:48:58 +0200

    cleaning code done

--- src/main/java/com/example/p3portaillocataireback/controller/RentalController.java
+++ src/main/java/com/example/p3portaillocataireback/controller/RentalController.java
@@ -23,16 +23,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/rentals")
